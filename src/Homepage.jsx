28b570d6594829a93ddf4c68401d5101d31e41f1
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-06-24 22:34:49 +0530

    Searchbox completed

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,26 +1,16 @@
 
 import './App.css'
-import Search_icon from './components/Other/SearchboxImg/search.png'
-import Location_icon from './components/Other/SearchboxImg/shape.png'
-import Date_icon from './components/Other/SearchboxImg/pinpaper-filled.png'
-import Members_icon from './components/Other/SearchboxImg/user.png'
+
+import Search from './components/Search_box/Search'
 
 function Homepage() {
 
   return (
     <>
       <div className='ImgDiv'> 
-
-        <div className='Search_div'>
-           <div className='Text_Slogan'>“Discover the world, <br /> &nbsp; keep the <span style={{color:'#B35950'}}>change”</span></div>
-           <div className='Search_box'>
-           <div style={{color:'Black'}}><img src={Location_icon} alt="" /><input type="text" name="Location" id="Location" placeholder='Location' /></div> 
-           <div><img src={Date_icon} alt="" /><input type="text" name="Date" id="Date" placeholder='Date'/></div> 
-           <div><img src={Members_icon} alt="" /><input type="text" name="Members" id="Members" placeholder='Members' /></div> 
-           <div><img src={Search_icon} alt="" /><input type="button" name="Search" id="Search"  value='Search'/>  </div> 
-           </div>
+        <Search/>
         </div>
-      </div>
+     
     </>
   )
 }
---
+++ src/components/Search_box/Search.jsx
@@ -0,0 +1,25 @@
+import Search_icon from '../Other/SearchboxImg/search.png'
+import Location_icon from '../Other/SearchboxImg/shape.png'
+import Date_icon from '../Other/SearchboxImg/pinpaper-filled.png'
+import Members_icon from '../Other/SearchboxImg/user.png'
+
+
+function Search(){
+    return(
+        <>
+        
+        <div className='Search_div'>
+           <div className='Text_Slogan'>“Discover the world, <br /> &nbsp; keep the <span style={{color:'#B35950'}}>change”</span></div>
+           <div className='Search_box'>
+           <div  className='Location'><img src={Location_icon} alt="location_icon" /><input type="text" name="Location" id="Location" placeholder='Location' /></div> 
+           <div className='Date'><img src={Date_icon} alt="Date_icon" /><input type="text" name="Date" id="Date" placeholder='Date'/></div> 
+           <div className='Members'><img src={Members_icon} alt="Members_icon" /><input type="text" name="Members" id="Members" placeholder='Members' /></div> 
+           <div className='Search' style={{background:'none'}}><img src={Search_icon} alt="Search_icon" /><input type="button" name="Search" id="Search"  value='Search'/>  </div> 
+           </div>
+           </div>
+        
+        </>
+    );
+}
+
+export default Search;
