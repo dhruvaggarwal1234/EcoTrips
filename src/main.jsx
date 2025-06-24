840c245717a8cc59cc893a3aaefc0d2dcf732f05
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-06-24 20:49:37 +0530

    searchbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import Homepage from './Homepage.jsx'
 import Navbar from './components/NAV/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
      <Navbar/>
-    <App />
+    <Homepage/>
   </StrictMode>,
 )
