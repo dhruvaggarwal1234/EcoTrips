82600644010ec210bbefcb3e093652d268352cf1
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-06-25 23:18:09 +0530

    weekend card

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,7 +3,8 @@ import Users from './components/Other/icon/users.png'
 import Heart from './components/Other/icon/heart.png'
 import Check from './components/Other/icon/check.png'
 import explore from './components/Other/icon/explore.png'
-
+import Kasoli from './components/Other/Card/Kasoli.png'
+import Star from './components/Other/Card/Star.png'
 import Search from './components/Search_box/Search'
 
 function Homepage() {
@@ -49,6 +50,27 @@ function Homepage() {
         <div className='Weekend_Div'>
           <p>Weekend</p>
           <p style={{color:'#897D7D' , fontSize:'2.45rem' ,fontWeight:"bold"}}>GateWays</p>
+        </div>
+
+        <div className='weekend_trip'>
+         <div className="Weekend_Card_main">
+          <div className='Weekend_Card_img'>
+            <div className='Star_div'><img src={Star} alt=""  className='Star' /></div> 
+
+            <div className='Trip_text'>
+              <div>
+               <span className='text_1'>Kausali, HP  <br /> </span>
+               <span className='text_2'>2 Days, 1 Night</span>
+              </div>
+
+              <div className='Marign_div'><br/>Exp. Chandigarh,Delhi</div>
+            </div>
+          </div>
+          <div className='Price_main'> 
+            <div className='Price_1'>RS 2499</div>
+            <div className='Price_2'><button className='Price_2_btn'>Book Now &gt; </button></div>
+           </div>
+         </div>
         </div>
       </section>
     </>
