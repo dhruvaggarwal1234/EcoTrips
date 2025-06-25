2366c1959ce5749bb37304e7c9f35abe86c3d91e
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-06-26 00:34:30 +0530

    weekend complete

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -3,9 +3,17 @@ import Users from './components/Other/icon/users.png'
 import Heart from './components/Other/icon/heart.png'
 import Check from './components/Other/icon/check.png'
 import explore from './components/Other/icon/explore.png'
-import Kasoli from './components/Other/Card/Kasoli.png'
-import Star from './components/Other/Card/Star.png'
+
 import Search from './components/Search_box/Search'
+import WCard from './components/Weekend_Card/WCard'
+import Kasoliimage from './components/Other/Card/Kausali.png'
+import Chailimage from "./components/Other/Card/Chail.png"
+import Chakrataimage from "./components/Other/Card/Chakrata.png"
+import Tehriimage from './components/Other/Card/Tehri.png'
+import MountAbuimage from './components/Other/Card/MountAbu.png'
+import Rishikeshimage from './components/Other/Card/Rishikesh.png'
+
+
 
 function Homepage() {
 
@@ -53,24 +61,16 @@ function Homepage() {
         </div>
 
         <div className='weekend_trip'>
-         <div className="Weekend_Card_main">
-          <div className='Weekend_Card_img'>
-            <div className='Star_div'><img src={Star} alt=""  className='Star' /></div> 
 
-            <div className='Trip_text'>
-              <div>
-               <span className='text_1'>Kausali, HP  <br /> </span>
-               <span className='text_2'>2 Days, 1 Night</span>
-              </div>
-
-              <div className='Marign_div'><br/>Exp. Chandigarh,Delhi</div>
-            </div>
-          </div>
-          <div className='Price_main'> 
-            <div className='Price_1'>RS 2499</div>
-            <div className='Price_2'><button className='Price_2_btn'>Book Now &gt; </button></div>
-           </div>
-         </div>
+          <WCard  Trip_name="Kausali, HP" Image={Kasoliimage}  Trip_price='2499'/>
+          <WCard  Trip_name="Chail, HP" Image={Chailimage}  Trip_price='2499'/>
+          <WCard  Trip_name="Chakrata, Uttarakhand" Image={Chakrataimage}  Trip_price='2499'/>
+          <WCard  Trip_name="New Tehri, Uttarakhand" Image={Tehriimage}  Trip_price='2999'/>
+          <WCard  Trip_name="MountAbu, Rajasthan" Image={MountAbuimage}  Trip_price='2999'/>
+          <WCard  Trip_name="Rishikesh, Uttarakhand" Image={Rishikeshimage}  Trip_price='1999'/>
+          
+         
+         
         </div>
       </section>
     </>
---
+++ src/components/Weekend_Card/WCard.jsx
@@ -0,0 +1,29 @@
+
+import Star from '../Other/Card/Star.png'
+
+
+function WCard({Trip_name ,Image , Trip_time="2 Days, 1 Night" , Trip_highway="Exp. Chandigarh,Delhi" ,Trip_price="2499"}){
+
+    return(
+        <div className="Weekend_Card_main">
+          <div className='Weekend_Card_img' style={{backgroundImage: `url(${Image})`}}>
+            <div className='Star_div'><img src={Star} alt=""  className='Star' /></div> 
+
+            <div className='Trip_text'>
+              <div>
+               <div className='text_1'> {Trip_name} <br /> </div>
+               <div className='text_2'>{Trip_time}</div>
+              </div>
+
+              <div className='Marign_div'><br/>{Trip_highway}</div>
+            </div>
+          </div>
+          <div className='Price_main'> 
+            <div className='Price_1'>RS {Trip_price}</div>
+            <div className='Price_2'><button className='Price_2_btn'>Book Now &gt; </button></div>
+           </div>
+         </div>
+    );
+}
+
+export default WCard;
