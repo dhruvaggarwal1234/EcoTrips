ffea431078e4fa3522a9d45c3dea997b91f6caa8
dhruvaggarwal1234 <dhruvaggarwal755@gmail.com>
2025-10-03 11:19:23 +0530

    Signup/Update

--- src/components/NAV/Model/Create.jsx
+++ src/components/NAV/Model/Create.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PhoneInput from "react-phone-input-2";
-import "react-phone-input-2/lib/style.css";  // default styles
+import "react-phone-input-2/lib/style.css";  
 import "./Create.css";
 
 function Create() {
@@ -12,25 +12,26 @@ function Create() {
         <label className="ml-label">Mobile Number</label>
 
         <PhoneInput
-          country={"in"}              // default India
+          country={"in"}              
           value={phone}
+          placeholder="Enter Your Mobile Number"
           onChange={setPhone}         // phone contains full value with dial code
           inputProps={{
             name: "phone",
             required: true,
-            autoFocus: false,
+            autoFocus: true,
           }}
           inputClass="phone-input"    // you can style this in CSS
           containerClass="phone-input-container"
         />
       </div>
 
-      <button className="login-button">CONTINUE</button>
+      <button className="login-button-c">CONTINUE</button>
 
-      <div className="or-row">
-        <div className="line" />
+      <div className="or-row-c">
+        <div className="line-c" />
         <div className="or-text">Or LogIn/SignUp With</div>
-        <div className="line" />
+        <div className="line-c" />
       </div>
 
       <div className="social-row">
@@ -38,7 +39,7 @@ function Create() {
         <button className="social-btn">M</button>
       </div>
 
-      <div className="footer-text">
+      <div className="footer-text-c">
         By proceeding, you agree to MakeMyTrip's{" "}
         <a href="#privacy">Privacy Policy</a>, <a href="#ua">User Agreement</a> and{" "}
         <a href="#tcs">T&Cs</a>
